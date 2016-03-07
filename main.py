c4cd2e4a2aa872fd8012603cc86df0d642a35778
pstahl <philip.stahl91@gmail.com>
2016-03-07 10:37:16 +0100

    handle selection of url

--- main.py
+++ main.py
@@ -13,20 +13,27 @@
 # 1. Install Python dependencies: cv2, flask. (wish that pip install works like a charm)
 # 2. Run "python main.py".
 # 3. Navigate the browser to the local webpage.
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, request
 from camera import VideoCamera
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('form.html')
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    return render_template('index.html', video_feed_url=request.form['url'])
+
 
 def gen(camera):
     while True:
         frame = camera.get_frame()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
 
 @app.route('/video_feed')
 def video_feed():
